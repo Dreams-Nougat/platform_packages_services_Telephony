1f9092f651b8043147db29a5995b3c541ee9a8e1
yan <yan1.yan@sonymobile.com>
2015-10-09 19:25:32 +0200

    Add status bar icon for HAC
    Display icon for HAC (Hearing Aid Compatibility) in status bar,
    when HAC is turned on.
    
    Test: Connect a HAC device and toggle on and off.
    Change-Id: Idfe217b496c22d0ee662aad54001747b6f3f1463

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -39,6 +39,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.SystemService;
 import android.os.UpdateLock;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -394,6 +395,11 @@ public class PhoneGlobals extends ContextWrapper {
             audioManager.setParameter(SettingsConstants.HAC_KEY,
                     hac == SettingsConstants.HAC_ENABLED
                             ? SettingsConstants.HAC_VAL_ON : SettingsConstants.HAC_VAL_OFF);
+            if (hac != 0) {
+                Intent hacModeChanged = new Intent(AudioManager.HAC_ENABLED_CHANGE_ACTION);
+                hacModeChanged.putExtra(AudioManager.EXTRA_HAC_ENABLED, true);
+                sendBroadcastAsUser(hacModeChanged, UserHandle.ALL);
+            }
         }
     }
 
